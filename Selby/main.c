0d69ece61a20b1f2cc6916f9f292debc76d5e1b2
Victor Freitas <victorf.santos@outlook.com>
2018-09-21 16:12:48 -0300

    Melhorei  a organização do codigo

--- Selby/main.c
+++ Selby/main.c
@@ -5,19 +5,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
 
-
-void mostra_caixa_de_texto(){
-    char tcaixa[50] = "Meu titulo";
-    //o título da mensagem dentro da caixa
-    char titulo[100] = "Sou uma caixa de texto";
-    //o conteúdo da mensagem exibida
-    char texto[200] = "A mensagem a ser exibida deve ficar aqui";
-    //mostra a caixa de texto
-    int r = al_show_native_message_box(NULL,tcaixa,titulo,texto,NULL,ALLEGRO_MESSAGEBOX_OK_CANCEL);
-    printf("%i",r);
-    return 0;
-
-}
+const int LARGURA_TELA = 1281;
+const int ALTURA_TELA = 721;
 
 void mensagem_de_erro(char *text){
 	al_show_native_message_box(NULL,"ERRO","Ocorreu o seguinte erro e o programa sera finalizado:",
@@ -25,23 +14,27 @@ void mensagem_de_erro(char *text){
 }
 
 void desenha_imagem(ALLEGRO_BITMAP *imagem){
-
     al_draw_bitmap(imagem,0, 0, 0); //Desenha imagem na tela
     al_flip_display(); //Atualiza o que está sendo mostrado na tea
-
-    //**neste lugar colocar evento "clique no inicio**"
-    al_rest(3.0); //Função que segura a execução por determinado tempo
     }
+
+void titulo_da_janela(ALLEGRO_DISPLAY *janela){
+    al_set_window_title(janela, "SelbySpace");
+}
+
+
 
 int main(){
 
     //VARIAVEIS
     ALLEGRO_DISPLAY *janela = NULL; //VAriavel tipo ALLEGRO_DISPLAY Representando Janela
-    ALLEGRO_BITMAP *tela_inicial = NULL; //Variavel tipo ALLEGRO_BITMAP Representando a imagem atual da tela
+    ALLEGRO_BITMAP *tela_inicial, *tela_X = NULL; //Variavel tipo ALLEGRO_BITMAP Representando a imagem atual da tela
     ALLEGRO_EVENT_QUEUE *fila_eventos = NULL; //Variavel tipo ALLEGRO_EVENT_QUEUE para fila de eventos
+    ALLEGRO_EVENT *evento = NULL;
+    int sair = 0; //Variavel para o loop
 
     //INICIALIZAÇÔES
-    if (!al_init()){        //Função que inicializa a lib allegro5
+    if (!al_init()){//Função que inicializa a lib allegro5
         mensagem_de_erro("Erro de inicialização do Allegro");
         return -1;
     }
@@ -51,12 +44,15 @@ int main(){
         return -1;
     }
 
-
-    janela = al_create_display(1280,720); //Função que cria o display com os parametros de tamanho em px
+    janela = al_create_display(LARGURA_TELA,ALTURA_TELA); //Função que cria o display com os parametros de tamanho em px
     if (!janela){
         mensagem_de_erro("Erro na inicialização do display");
         return -1;
     }
+
+
+    titulo_da_janela(janela); //Função que define o titulo da janela
+
 
     tela_inicial = al_load_bitmap("res/img/Menu.png");//Função que carrega imagem para variavel
     if (!tela_inicial){
@@ -70,7 +66,22 @@ int main(){
         return -1;
     }
 
+    if (!al_install_mouse()){//Função que instala a funcionalidade de Mouse
+    mensagem_de_erro("Erro ao instalar mouse.");
+    return -1;
+    }
+
+    // Atribui o cursor padrão do sistema para ser usado
+    if (!al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT)){
+        mensagem_de_erro("Erro ao atribuir cursor ao sistema");
+        return -1;
+    }
+
     desenha_imagem(tela_inicial);
+
+
+     //**neste lugar colocar evento "clique no inicio**"
+    al_rest(3.0); //Função que segura a execução por determinado tempo
 
 
 
