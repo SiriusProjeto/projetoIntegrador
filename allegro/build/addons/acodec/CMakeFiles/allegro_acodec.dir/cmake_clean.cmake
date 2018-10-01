aad744978f71af8f7e35c6609b413bb32c773b8a
Victor Freitas <victorf.santos@outlook.com>
2018-10-01 15:42:38 -0300

    adc files

---
+++ allegro/build/addons/acodec/CMakeFiles/allegro_acodec.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/allegro_acodec.dir/acodec.c.o"
+  "CMakeFiles/allegro_acodec.dir/wav.c.o"
+  "CMakeFiles/allegro_acodec.dir/helper.c.o"
+  "CMakeFiles/allegro_acodec.dir/flac.c.o"
+  "CMakeFiles/allegro_acodec.dir/modaudio.c.o"
+  "CMakeFiles/allegro_acodec.dir/ogg.c.o"
+  "../../lib/liballegro_acodec.pdb"
+  "../../lib/liballegro_acodec.so.5.0.10"
+  "../../lib/liballegro_acodec.so"
+  "../../lib/liballegro_acodec.so.5.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/allegro_acodec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
