bdd9ca61cd88bbaeebd3c36097a0d6112621c0e1
JMelo <41171438+Josuka@users.noreply.github.com>
2018-12-03 19:00:35 -0200

    Update funcoes_basicas.h

--- Selby/scripts/funcoes_basicas.h
+++ Selby/scripts/funcoes_basicas.h
@@ -227,10 +227,75 @@ bool inicializar(){
         al_shutdown_primitives_addon();
         return false;
     }
-
-    apresentacao[3] = al_load_bitmap("res/img/inicio/inicioJogo4.jpeg");
+apresentacao[3] = al_load_bitmap("res/img/inicio/inicioJogo4.jpeg");
+    if(!apresentacao[3]){
+        fprintf(stderr, "Falha ao carregar tela de apresentaçao.\n");
+        al_destroy_bitmap(apresentacao[3]);
+        al_destroy_bitmap(apresentacao[2]);
+        al_destroy_bitmap(apresentacao[1]);
+        al_destroy_bitmap(apresentacao[0]);
+        al_destroy_bitmap(boas_vindas[0]);
+        al_destroy_bitmap(boas_vindas[1]);
+        al_destroy_bitmap(boas_vindas[2]);
+        al_destroy_bitmap(tela_inicio);
+        al_destroy_event_queue(fila_eventos);
+        al_destroy_font(fonte);
+        al_destroy_display(janela);
+        al_uninstall_mouse();
+        al_uninstall_audio();
+        al_shutdown_image_addon();
+        al_shutdown_ttf_addon();
+        al_shutdown_font_addon();
+        al_shutdown_primitives_addon();
+        return false;
+    }
     apresentacao[4] = al_load_bitmap("res/img/inicio/inicioJogo5.jpeg");
-    apresentacao[5] = al_load_bitmap("res/img/inicio/inicioJogo6.jpeg");
+        if(!apresentacao[4]){
+        fprintf(stderr, "Falha ao carregar tela de apresentaçao.\n");
+        al_destroy_bitmap(apresentacao[4]);
+        al_destroy_bitmap(apresentacao[3]);
+        al_destroy_bitmap(apresentacao[2]);
+        al_destroy_bitmap(apresentacao[1]);
+        al_destroy_bitmap(apresentacao[0]);
+        al_destroy_bitmap(boas_vindas[0]);
+        al_destroy_bitmap(boas_vindas[1]);
+        al_destroy_bitmap(boas_vindas[2]);
+        al_destroy_bitmap(tela_inicio);
+        al_destroy_event_queue(fila_eventos);
+        al_destroy_font(fonte);
+        al_destroy_display(janela);
+        al_uninstall_mouse();
+        al_uninstall_audio();
+        al_shutdown_image_addon();
+        al_shutdown_ttf_addon();
+        al_shutdown_font_addon();
+        al_shutdown_primitives_addon();
+        return false;
+    }
+        apresentacao[5] = al_load_bitmap("res/img/inicio/inicioJogo6.jpeg");
+        if(!apresentacao[5]){
+        fprintf(stderr, "Falha ao carregar tela de apresentaçao.\n");
+        al_destroy_bitmap(apresentacao[5]);
+        al_destroy_bitmap(apresentacao[4]);
+        al_destroy_bitmap(apresentacao[3]);
+        al_destroy_bitmap(apresentacao[2]);
+        al_destroy_bitmap(apresentacao[1]);
+        al_destroy_bitmap(apresentacao[0]);
+        al_destroy_bitmap(boas_vindas[0]);
+        al_destroy_bitmap(boas_vindas[1]);
+        al_destroy_bitmap(boas_vindas[2]);
+        al_destroy_bitmap(tela_inicio);
+        al_destroy_event_queue(fila_eventos);
+        al_destroy_font(fonte);
+        al_destroy_display(janela);
+        al_uninstall_mouse();
+        al_uninstall_audio();
+        al_shutdown_image_addon();
+        al_shutdown_ttf_addon();
+        al_shutdown_font_addon();
+        al_shutdown_primitives_addon();
+        return false;
+    }
     apresentacao[6] = al_load_bitmap("res/img/inicio/inicioJogo7.jpeg");
     apresentacao[7] = al_load_bitmap("res/img/inicio/inicioJogo8.jpeg");
     apresentacao[8] = al_load_bitmap("res/img/inicio/inicioJogo9.jpeg");
@@ -239,6 +304,32 @@ bool inicializar(){
     transicaoTerra = al_load_bitmap("res/img/transicoes_de_tela/avaliacaoVenus.jpg");
     explicaTerra[0] = al_load_bitmap("res/img/fase_Terra/explicacaoTerra1.jpg");
     explicaTerra[1] = al_load_bitmap("res/img/fase_Terra/explicacaoTerra2.jpg");
+
+
+    acre = al_load_bitmap("res/img/fase_Terra/estados/acre.png");
+    alagoas = al_load_bitmap("res/img/fase_Terra/estados/alagoas.png");
+    amapa = al_load_bitmap("res/img/fase_Terra/estados/amapa.png");
+    amazonas = al_load_bitmap("res/img/fase_Terra/estados/amazonas.png");
+    bahia = al_load_bitmap("res/img/fase_Terra/estados/bahia.png");
+    ceara = al_load_bitmap("res/img/fase_Terra/estados/ceara.png");
+    espirito_santo = al_load_bitmap("res/img/fase_Terra/estados/espirito_santo.png");
+    goias = al_load_bitmap("res/img/fase_Terra/estados/goias.png");
+    maranhao = al_load_bitmap("res/img/fase_Terra/estados/maranhao.png");
+    mato_grosso = al_load_bitmap("res/img/fase_Terra/estados/mato_grosso.png");
+    mato_grosso_do_sul = al_load_bitmap("res/img/fase_Terra/estados/mato_grosso_do_sul.png");
+    minas_gerais = al_load_bitmap("res/img/fase_Terra/estados/minas_gerais.png");
+    paraiba = al_load_bitmap("res/img/fase_Terra/estados/paraiba.png");
+    parana = al_load_bitmap("res/img/fase_Terra/estados/parana.png");
+    pernambuco = al_load_bitmap("res/img/fase_Terra/estados/pernambuco.png");
+    piaui = al_load_bitmap("res/img/fase_Terra/estados/piaui.png");
+    rio_de_janeiro = al_load_bitmap("res/img/fase_Terra/estados/rio_de_janeiro.png");
+    rio_grande_do_norte = al_load_bitmap("res/img/fase_Terra/estados/rio_grande_do_norte.png");
+    rio_grande_do_sul = al_load_bitmap("res/img/fase_Terra/estados/rio_grande_do_sul.png");
+    roraima = al_load_bitmap("res/img/fase_Terra/estados/roraima.png");
+    santa_catarina = al_load_bitmap("res/img/fase_Terra/estados/santa_catarina.png");
+    sao_paulo = al_load_bitmap("res/img/fase_Terra/estados/sao_paulo.png");
+    sergipe = al_load_bitmap("res/img/fase_Terra/estados/sergipe.png");
+    tocantins = al_load_bitmap("res/img/fase_Terra/estados/tocantins.png");
 
     if (!al_install_keyboard()){
         fprintf(stderr, "Falha ao inicializar o teclado.\n");
