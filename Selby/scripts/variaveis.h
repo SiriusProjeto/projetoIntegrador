acbcd5e5da9986606581066303b462192c2e4d57
JMelo <41171438+Josuka@users.noreply.github.com>
2018-12-03 18:58:25 -0200

    Update variaveis.h

--- Selby/scripts/variaveis.h
+++ Selby/scripts/variaveis.h
@@ -14,10 +14,35 @@ ALLEGRO_BITMAP *boas_vindas[3] = {NULL, NULL, NULL};
 ALLEGRO_BITMAP *boas_vindas_alerta = NULL;
 ALLEGRO_BITMAP *apresentacao[10] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
+ALLEGRO_BITMAP *acre = NULL;
+ALLEGRO_BITMAP *alagoas = NULL;
+ALLEGRO_BITMAP *amapa = NULL;
+ALLEGRO_BITMAP *amazonas = NULL;
+ALLEGRO_BITMAP *bahia = NULL;
+ALLEGRO_BITMAP *ceara = NULL;
+ALLEGRO_BITMAP *espirito_santo = NULL;
+ALLEGRO_BITMAP *goias = NULL;
+ALLEGRO_BITMAP *maranhao = NULL;
+ALLEGRO_BITMAP *mato_grosso = NULL;
+ALLEGRO_BITMAP *mato_grosso_do_sul = NULL;
+ALLEGRO_BITMAP *minas_gerais = NULL;
+ALLEGRO_BITMAP *paraiba = NULL;
+ALLEGRO_BITMAP *parana = NULL;
+ALLEGRO_BITMAP *pernambuco = NULL;
+ALLEGRO_BITMAP *piaui = NULL;
+ALLEGRO_BITMAP *rio_de_janeiro = NULL;
+ALLEGRO_BITMAP *rio_grande_do_norte = NULL;
+ALLEGRO_BITMAP *rio_grande_do_sul = NULL;
+ALLEGRO_BITMAP *rondonia = NULL;
+ALLEGRO_BITMAP *roraima = NULL;
+ALLEGRO_BITMAP *santa_catarina = NULL;
+ALLEGRO_BITMAP *sao_paulo = NULL;
+ALLEGRO_BITMAP *sergipe = NULL;
+ALLEGRO_BITMAP *tocantins = NULL;
+
 ALLEGRO_BITMAP *venus = NULL;
 ALLEGRO_BITMAP *transicaoTerra = NULL;
 
 ALLEGRO_BITMAP *explicaTerra[2] = {NULL, NULL};
-
 
 char palavra[15];
