949e362e02b65ad4bdd7caf283ac675c2b6be6a4
josue <josue.melomatos@gmail.com>
2018-12-10 19:28:10 -0200

    projwto

--- Selby/scripts/definicoes.h
+++
@@ -1,9 +0,0 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
-#include <stdio.h>
-#include <stdbool.h>
